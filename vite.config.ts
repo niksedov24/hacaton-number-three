3e31edfa837cb8dd0424a71b29c3b809c32647d8
niksedov24 <nik.sedov890@gmail.com>
2026-03-18 20:30:10 +0400

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
-  base: process.env.VITE_BASE_PATH || "/hacaton-number-three",
+  base: "/hacaton-number-three/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
